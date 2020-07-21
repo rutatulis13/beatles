facbfa9b37cd17053bb8055166181e1b3869c046
rutatulis13 <zuke.ruta@gmail.com>
2020-07-21 20:21:22 +0300

    message

--- beatles.js
+++ beatles.js
@@ -1,7 +1,7 @@
-  function changePhoto()
-{
-  const e = document.getElementById("photos");
-  const chooseImage = e.options[e.selectedIndex].value;
-  console.log(chooseImage);
-  document.getElementById("imagesimages").style.backgroundImage = "url("+chooseImage+")";
-} 
+document.getElementById("photos").addEventListener("change", changePicture)
+
+	function changePicture () {
+	const e = document.getElementById("photos");
+  	const chooseImage = e.options[e.selectedIndex].value;
+  	document.getElementById("imagesimages").style.backgroundImage = "url("+chooseImage+")";
+}
